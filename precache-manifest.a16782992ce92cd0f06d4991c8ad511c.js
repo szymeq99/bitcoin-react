fc1068459efdf370ba9ae2d242faf84f7b559b53
Szymon Krok <szymon-89@tlen.pl>
2019-11-17 16:40:41 +0100

    Updates

---
+++ precache-manifest.a16782992ce92cd0f06d4991c8ad511c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fb168dbdd76517e42d8460837423ad8",
+    "url": "/bitcoin-react/index.html"
+  },
+  {
+    "revision": "ef6d65faaa3396d8a5c0",
+    "url": "/bitcoin-react/static/css/main.25aa840c.chunk.css"
+  },
+  {
+    "revision": "7aecbb5972906c44f9d5",
+    "url": "/bitcoin-react/static/js/2.3682e782.chunk.js"
+  },
+  {
+    "revision": "ef6d65faaa3396d8a5c0",
+    "url": "/bitcoin-react/static/js/main.5f34db94.chunk.js"
+  },
+  {
+    "revision": "ef8e12efa0b888512344",
+    "url": "/bitcoin-react/static/js/runtime-main.7fea3221.js"
+  }
+]);
